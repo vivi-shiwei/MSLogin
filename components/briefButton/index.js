6e68e6424148dd72dd14ea897b6a6791040d359a
vivi-shiwei <2546710985@qq.com>
2020-06-06 18:21:34 +0800

    6-6-18-21

--- components/briefButton/index.js
+++ components/briefButton/index.js
@@ -11,33 +11,35 @@ import {
 
 const briefButton = ({ redirectPath }) => {
   return (
-    <Container>
-      <Input type="tel" placeholder="123456" borderRadius='4px' fontSize="5xl" h />
-      <Box d='flex' flexWrap='wrap' justifyContent='center'
-        pt={2}>
-        <Button
-          as='a'
-          href="/"
-          size='lg'
-          w='100%'
-          variantColor='teal'
-          variant='solid'
-          borderRadius='9999px'
-          color='#fff'
-          background='rgb(179, 198, 255)'
-          p={8}
-        >
-          <Box
-            fontSize="2xl"
-            d='flex'
-            flex='1 0 auto'
-            justifyContent='center'
-            mt='-1px'
-            ml='8px'
-          >確定</Box>
-        </Button>
-      </Box>
-    </Container>
+    <>
+      <Input type="tel" placeholder="123456" borderRadius='4px' fontSize="5xl" w={{ base: "100%", sm: '440px' }} h />
+      <Container>
+        <Box d='flex' flexWrap='wrap' justifyContent='center'
+          pt={2}>
+          <Button
+            as='a'
+            href="/"
+            size='lg'
+            w='100%'
+            variantColor='teal'
+            variant='solid'
+            borderRadius='9999px'
+            color='#fff'
+            background='rgb(179, 198, 255)'
+            p={8}
+          >
+            <Box
+              fontSize="2xl"
+              d='flex'
+              flex='1 0 auto'
+              justifyContent='center'
+              mt='-1px'
+              ml='8px'
+            >確定</Box>
+          </Button>
+        </Box>
+      </Container>
+    </>
   )
 }
 
--- stories/phone.stories.js
+++ stories/phone.stories.js
@@ -23,35 +23,12 @@ import {
     Label,
 } from './style'
 
-const Containers = (props) => {
-    return (
-        <SimpleGrid
-            columns={2}
-            spacing='16px'
-            py='16px'
-            display={{ base: 'block', sm: 'flex' }}
-            w={{ base: '100%', sm: '550px' }}
-            {...props}
-        />
-    )
-}
-
-
 const phone = ({ redirectPath }) => {
     return (
         <Layout>
             <FullscreenView>
                 <FullscreenContent justifyContent='center'>
                     <LargeTitle>手機登入</LargeTitle>
-                    <InputGroup pt={6}>
-                        <Select placeholder="澳門(+853)" fontSize="2xl" h="60px" m={1}
-                            borderRadius='10px'>
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3">Option 3</option>
-                        </Select>
-                        <Input type="tel" placeholder="輸出你的手機號碼" borderRadius='10px' fontSize="2xl" m={1} h="60px" />
-                    </InputGroup>
                     <PhoneButton />
                 </FullscreenContent>
             </FullscreenView>
